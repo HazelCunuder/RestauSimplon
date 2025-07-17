0557dad392b0b8e685594624452797d4ed2282f2
HazelCunuder <basile.cunuder@gmail.com>
2025-07-17 11:51:43 +0200

    feat: add gestion groupe commande

---
+++ GroupCommande/GroupCommandeDTO.cs
@@ -0,0 +1,23 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+
+namespace RestauSimplon.GestionClient
+{
+    public class GroupCommandeDTO
+    {
+        [SwaggerSchema("Id du regroupement")]
+        public int Id { get; set; }
+
+        [SwaggerSchema("Identifiant du plat")]
+        public int IdCommande { get; set; }
+
+        [SwaggerSchema("Identifiant du plat")]
+        public int IdPlat { get; set; }
+
+        [SwaggerSchema("Quantité commandée")]
+        public int Quantite { get; set; }
+
+        public GroupCommandeDTO() { }
+        public GroupCommandeDTO(GroupCommande groupe) =>
+            (Id, IdCommande, IdPlat, Quantite) = (groupe.Id, groupe.IdCommande, groupe.IdPlat, groupe.Quantite);
+    }
+}

640343bf14181ece787a9f36b806693d6778c6be
Hazel Cunuder <122833349+HazelCunuder@users.noreply.github.com>
2025-07-17 09:42:55 +0200

    Feature/gestion client (#4)
    * feat(gestion-client): add class to deserialize json files and add them to the db
    
    * feat(gestion-clients): add class to handle Clients Endpoints
    
    * feat(Gestion-clients): add template for the db
    
    * feat: update gitignore to include .db file and Migrations Folder
    
    * feat: add Swagger

---
+++ GestionClient/ClientsDTO.cs
@@ -0,0 +1,26 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+
+namespace RestauSimplon.GestionClient
+{
+    public class ClientsDTO
+    {
+        [SwaggerSchema("Id du client")]
+        public int Id { get; set; }
+
+        [SwaggerSchema("Nom de Famille du client")]
+        public string Nom { get; set; }
+
+        [SwaggerSchema("Prenom du client")]
+        public string Prenom { get; set; }
+
+        [SwaggerSchema("Adresse du client")]
+        public string? Adresse { get; set; }
+
+        [SwaggerSchema("Numéro de téléphone du client")]
+        public string? Phone { get; set; }
+
+        public ClientsDTO() { }
+        public ClientsDTO(Clients clients) =>
+            (Id, Nom, Prenom, Adresse, Phone) = (clients.Id, clients.Nom, clients.Prenom, clients.Adresse, clients.Phone);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using RestauSimplon.Data_Existante;
+using RestauSimplon.Endpoints;
+using RestauSimplon.GestionClient;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Numerics;
+
 namespace RestauSimplon
 {
     public class Program
@@ -5,9 +13,49 @@ namespace RestauSimplon
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // -- Ajout de la BDD SQLite avec les infos de clients --
+            builder.Services.AddDbContext<ClientsDb>(opt => opt.UseSqlite("Data Source=ClientsDb.db"));
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "RestauSimplon",
+                    Version = "v1",
+                    Description = "Une API pour gérer les clients et commandes du restaurant",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Groupe Best",
+                        Email = "lesbest@gmail.com"
+                    }
+                });
+
+                c.EnableAnnotations();
+            });
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            // -- Ajout de clients.json dans la BDD --
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<ClientsDb>();
+                Task.Run(() => ClientSeeder.SeedAsync(db)).Wait();
+            }
+
+            // -- Appelle la méthode permettant de générer les endpoints de /clients --
+            app.MapGroup("/clients").MapClientsEndpoints();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "RestauSimplon v1");
+                    c.RoutePrefix = "";
+                });
+            }
 
             app.Run();
         }

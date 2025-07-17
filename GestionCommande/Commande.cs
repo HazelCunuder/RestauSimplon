b3fa5d10a69d19245b6fd477d608a442c5335797
Vincent <faivre.vinz@gmail.com>
2025-07-17 14:04:15 +0200

    feat(gestioncommande): add gestioncommande

---
+++ GestionCommande/Commande.cs
@@ -0,0 +1,19 @@
+﻿public class Commande
+{
+
+    // -- Créer les propriétés de la table commande
+    public int Id { get; set; }
+    public decimal PrixTotal { get; set; }
+    public bool StatutCommande { get; set; }
+    public DateTime DateCommande { get; set; } 
+    
+    // Constructeur
+    public Commande()
+    {
+        // Date actuelle par défaut
+        DateCommande = DateTime.Now;
+
+        // Commande non terminée par défaut
+        StatutCommande = false;
+    }
+}

8deff26697b4a36717f7b72ca8cb0aa9c7d33782
HazelCunuder <basile.cunuder@gmail.com>
2025-07-17 09:32:46 +0200

    feat(Gestion-clients): add template for the db

---
+++ GestionClient/Clients.cs
@@ -0,0 +1,13 @@
+﻿namespace RestauSimplon.GestionClient
+{
+    public class Clients
+    {
+        // -- Créer les propriétés de la table Clients --
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string? Adresse { get; set; }
+        public string? Phone { get; set; }
+        public string? Secret { get; set; }
+    }
+}

750736227f319b0a5a8a4572219247d40ce60a71
AmineBenfetta <amine.benfetta711@gmail.com>
2025-07-18 12:14:19 +0200

    feat(validation): add validation to article section.

--- GestionArticle/ArticleDTO.cs
+++ GestionArticle/ArticleDTO.cs
@@ -1,4 +1,5 @@
 ﻿using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace RestauSimplon.GestionArticle
 {
@@ -7,12 +8,18 @@ namespace RestauSimplon.GestionArticle
         [SwaggerSchema("Id de l'article")]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Un nom de plat valide est requis.")]
+        [MinLength(2, ErrorMessage = "Le nom doit contenir au moins 2 caractères.")]
         [SwaggerSchema("Nom du plat")]
         public string Nom { get; set; }
-
+        
+        [Required(ErrorMessage = "Une categorie valide est requise.")]
+        [MinLength(2, ErrorMessage = "La categorie doit contenir au moins 2 caractères.")]
         [SwaggerSchema("Categorie du plat")]
         public string Categorie { get; set; }
 
+        [Required(ErrorMessage = "Un prix est requis.")]
+        [Range(0.01, 1000, ErrorMessage = "Le prix doit être compris entre 0.01 et 1000.")]
         [SwaggerSchema("Prix du plat")]
         public double Prix { get; set; }
 

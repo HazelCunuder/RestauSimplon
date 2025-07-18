fd10e33aff0f052c7a840a95773cc755d2251141
AmineBenfetta <amine.benfetta711@gmail.com>
2025-07-18 13:53:19 +0200

    feat(validation): add regex to articles

--- GestionArticle/ArticleDTO.cs
+++ GestionArticle/ArticleDTO.cs
@@ -10,11 +10,13 @@ namespace RestauSimplon.GestionArticle
 
         [Required(ErrorMessage = "Un nom de plat valide est requis.")]
         [MinLength(2, ErrorMessage = "Le nom doit contenir au moins 2 caractères.")]
+        [RegularExpression(@"^[A-Za-zÀ-ÿ\s\-']+$", ErrorMessage = "Le nom ne doit contenir que des lettres, espaces, tirets ou apostrophes")]
         [SwaggerSchema("Nom du plat")]
         public string Nom { get; set; }
         
         [Required(ErrorMessage = "Une categorie valide est requise.")]
         [MinLength(2, ErrorMessage = "La categorie doit contenir au moins 2 caractères.")]
+        [RegularExpression(@"^[A-Za-z]+$", ErrorMessage = "La categorie doit contenir uniquement des lettres sans espaces")]
         [SwaggerSchema("Categorie du plat")]
         public string Categorie { get; set; }
 

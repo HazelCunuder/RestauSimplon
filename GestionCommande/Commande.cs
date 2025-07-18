c6f502e89a62be626939995574b4515e690bde66
Vincent <faivre.vinz@gmail.com>
2025-07-18 13:56:13 +0200

    <feature>(GestionCommande): Add "Creation de commande" in GroupEndpoints.

--- GestionCommande/Commande.cs
+++ GestionCommande/Commande.cs
@@ -1,11 +1,18 @@
-﻿public class Commande
-{
+﻿using RestauSimplon.GestionClient;
+using System.ComponentModel.DataAnnotations.Schema;
 
+public class Commande
+{
     // -- Créer les propriétés de la table commande
     public int Id { get; set; }
     public decimal PrixTotal { get; set; }
     public bool StatutCommande { get; set; }
-    public DateTime DateCommande { get; set; } 
+    public DateTime DateCommande { get; set; }
+
+    [ForeignKey("IdClient")]
+    public int IdClient { get; set; }
+
+    public List<GroupCommande> lignes { get; set; } = new();
     
     // Constructeur
     public Commande()
--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace RestauSimplon
 
             builder.Services.AddDbContext<GroupCommandeDb>(opt => opt.UseSqlite("Data Source=GroupCommandeDb.db"));
 
-            builder.Services.AddDbContext<ArticleDb>(opt => opt.UseSqlite("Data Source=ArticlesDb.db"));
+            builder.Services.AddDbContext<ArticleDb>(opt => opt.UseSqlite("Data Source=ArticleDb.db"));
 
             builder.Services.AddDbContext<CommandeDb>(opt => opt.UseSqlite("Data Source=CommandeDb.db"));
 
@@ -48,9 +48,6 @@ using (var scope = app.Services.CreateScope())
     var db = scope.ServiceProvider.GetRequiredService<ArticleDb>();
     Task.Run(() => ArticleSeeder.SeedAsync(db)).Wait();
 }
-
-
-
             // -- Ajout de clients.json dans la BDD --
             using (var scope = app.Services.CreateScope())
             {
@@ -65,7 +62,7 @@ using (var scope = app.Services.CreateScope())
             app.MapGroup("/articles").MapArticleEndpoints();
 
             // -- Appelle la méthode permettant de générer les endpoints de /commandes --
-            app.MapGroup("/commandes").MapArticleEndpoints();
+            app.MapGroup("/commandes").MapCommandeEndpoints();
 
             // -- Appelle la méthode permettant de générer les endpoints de /groupes-commandes --
             app.MapGroup("/groupe-commandes").MapGroupEndpoints();
